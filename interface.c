010bcb58529ea6b06813ad437468400ec4b157e3
ZhongRuoyu <zhongruoyu@outlook.com>
2021-05-19 15:46:10 +0800

    Memory leak fixed

--- interface.c
+++ interface.c
@@ -18,6 +18,7 @@ char *cipher_hex(unsigned Nk, const char *key, const char *in) {
 
     char *in_processed = process_hex_string(in);
     if (strlen(in_processed) != 32) {
+        free(in_processed);
         for (unsigned i = 0; i < Nr + 1; ++i) free(key_processed[i]);
         free(key_processed);
         error("Incorrect input length.", NULL);
@@ -41,6 +42,7 @@ char *inv_cipher_hex(unsigned Nk, const char *key, const char *in) {
 
     char *in_processed = process_hex_string(in);
     if (strlen(in_processed) != 32) {
+        free(in_processed);
         for (unsigned i = 0; i < Nr + 1; ++i) free(key_processed[i]);
         free(key_processed);
         error("Incorrect input length.", NULL);
@@ -85,6 +87,7 @@ char *inv_cipher_hex_multiblock(unsigned Nk, const char *key, const char *in) {
     char *in_processed = process_hex_string(in);
     const unsigned n = strlen(in_processed);
     if (n % 32) {
+        free(in_processed);
         for (unsigned i = 0; i < Nr + 1; ++i) free(key_processed[i]);
         free(key_processed);
         error("Incorrect input length.", NULL);
