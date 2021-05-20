38f0a1df3f4c7ac33522fce1abae17e8ef31d801
ZhongRuoyu <zhongruoyu@outlook.com>
2021-05-20 10:27:37 +0800

    Code simplified

--- cipher.c
+++ cipher.c
@@ -250,14 +250,10 @@ static void InvShiftRows(unsigned Nb, byte state[]) {
 static void MixColumns(unsigned Nb, byte state[]) {
     byte *new_state = (byte *)malloc(4 * Nb * sizeof(byte));
     for (unsigned j = 0; j < Nb; ++j) {
-        const byte r[4] = {state[0 * Nb + j],
-                           state[1 * Nb + j],
-                           state[2 * Nb + j],
-                           state[3 * Nb + j]};
         for (unsigned i = 0; i < 4; ++i) {
             byte product = 0;
             for (unsigned k = 0; k < 4; ++k) {
-                product ^= multiply(r[k], MixColumns_multiplier[(4 - i + k) % 4]);
+                product ^= multiply(state[k * Nb + j], MixColumns_multiplier[(4 - i + k) % 4]);
             }
             new_state[i * Nb + j] = product;
         }
@@ -269,14 +265,10 @@ static void MixColumns(unsigned Nb, byte state[]) {
 static void InvMixColumns(unsigned Nb, byte state[]) {
     byte *new_state = (byte *)malloc(4 * Nb * sizeof(byte));
     for (unsigned j = 0; j < Nb; ++j) {
-        const byte r[4] = {state[0 * Nb + j],
-                           state[1 * Nb + j],
-                           state[2 * Nb + j],
-                           state[3 * Nb + j]};
         for (unsigned i = 0; i < 4; ++i) {
             byte product = 0;
             for (unsigned k = 0; k < 4; ++k) {
-                product ^= multiply(r[k], InvMixColumns_multiplier[(4 - i + k) % 4]);
+                product ^= multiply(state[k * Nb + j], InvMixColumns_multiplier[(4 - i + k) % 4]);
             }
             new_state[i * Nb + j] = product;
         }
