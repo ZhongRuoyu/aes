07e539ba31b81b41c5a061fbc064673d51d0db0a
ZhongRuoyu <zhongruoyu@outlook.com>
2021-05-24 13:46:11 +0800

    Data generation program optimised

--- data/makedata.c
+++ data/makedata.c
@@ -13,9 +13,6 @@ typedef union uword {
     byte bytes[4];
 } uword;
 
-static word s_box[256];
-static word inverse_s_box[256];
-
 static byte multiply(byte a, byte b) {
     byte res = 0;
     for (; b; b >>= 1) {
@@ -41,13 +38,13 @@ static byte SubByte(byte b) {
     return d ^ d >> 4 ^ d >> 5 ^ d >> 6 ^ d >> 7 ^ 0x63;
 }
 
-static void initialise_s_box(void) {
+static void initialise_s_box(word s_box[]) {
     for (size_t i = 0; i < 256; ++i) {
         s_box[i] = SubByte((byte)i);
     }
 }
 
-static void initialise_inverse_s_box(void) {
+static void initialise_inverse_s_box(word inverse_s_box[]) {
     for (size_t i = 0; i < 256; ++i) {
         inverse_s_box[SubByte((byte)i)] = (byte)i;
     }
@@ -59,33 +56,26 @@ static void print_Rcon(FILE *file) {
     // i in Rcon[i] can be up to (Nb * (Nr + 1)) / Nk (exclusive).
     // For Nb = 8, Nk = 4, Nr = 14, that is up to 30 (exclusive).
     const unsigned MaxRcon = 30;
-    byte RC = 0x01;
+    uword c = {0x00000001};
 
     fprintf(file, "    // Rcon[0] is included for indexing simplicity\n");
     fprintf(file, "    0x00000000,\n");
 
     for (unsigned i = 1; i < MaxRcon; ++i) {
-        fprintf(file, "    0x000000%02x,\n", RC);
-        RC = multiply(0x02, RC);
+        fprintf(file, "    0x%08x,\n", c.word);
+        c.bytes[0] = multiply(0x02, c.bytes[0]);
     }
 
     fprintf(file, "};\n");
 }
 
-static void print_s_box(FILE *file) {
-    fprintf(file, "const word s_box[4][256] = {\n");
-
-    const char *fmt[4] = {
-        "0x000000%02x",
-        "0x0000%02x00",
-        "0x00%02x0000",
-        "0x%02x000000",
-    };
+static void print_s_box(FILE *file, const char *name, const word s_box[]) {
+    fprintf(file, "const word %s[4][256] = {\n", name);
 
     for (unsigned i = 0; i < 4; ++i) {
         fprintf(file, "    {");
         for (unsigned j = 0; j < 256; ++j) {
-            fprintf(file, fmt[i], s_box[j]);
+            fprintf(file, "0x%08x", s_box[j] << (8 * i));
             if (j != 255) fprintf(file, ", ");
         }
         fprintf(file, "},\n");
@@ -94,39 +84,16 @@ static void print_s_box(FILE *file) {
     fprintf(file, "};\n");
 }
 
-static void print_inverse_s_box(FILE *file) {
-    fprintf(file, "const word inverse_s_box[4][256] = {\n");
-
-    const char *fmt[4] = {
-        "0x000000%02x",
-        "0x0000%02x00",
-        "0x00%02x0000",
-        "0x%02x000000",
-    };
-
-    for (unsigned i = 0; i < 4; ++i) {
-        fprintf(file, "    {");
-        for (unsigned j = 0; j < 256; ++j) {
-            fprintf(file, fmt[i], inverse_s_box[j]);
-            if (j != 255) fprintf(file, ", ");
-        }
-        fprintf(file, "},\n");
-    }
-
-    fprintf(file, "};\n");
-}
-
-static void print_cipher_table(FILE *file) {
-    fprintf(file, "const word cipher_table[4][256] = {\n");
+static void print_table(FILE *file, const char *name, const word s_box[], const word m[]) {
+    fprintf(file, "const word %s[4][256] = {\n", name);
 
     byte t[4][256];
-    const word m[4] = {0x03, 0x01, 0x01, 0x02};
-
     for (unsigned i = 0; i < 4; ++i) {
         for (unsigned j = 0; j < 256; ++j) {
             t[i][j] = multiply(m[i % 4], j);
         }
     }
+    
     for (unsigned i = 0; i < 4; ++i) {
         fprintf(file, "    {");
         for (unsigned j = 0; j < 256; ++j) {
@@ -143,46 +110,17 @@ static void print_cipher_table(FILE *file) {
     fprintf(file, "};\n");
 }
 
-static void print_inv_cipher_table(FILE *file) {
-    fprintf(file, "const word inv_cipher_table[4][256] = {\n");
-
-    byte t[4][256];
-    const word m[4] = {0x0b, 0x0d, 0x09, 0x0e};
-
-    for (unsigned i = 0; i < 4; ++i) {
-        for (unsigned j = 0; j < 256; ++j) {
-            t[i][j] = multiply(m[i % 4], j);
-        }
-    }
-    for (unsigned i = 0; i < 4; ++i) {
-        fprintf(file, "    {");
-        for (unsigned j = 0; j < 256; ++j) {
-            fprintf(file, "0x%02x%02x%02x%02x",
-                    t[(i + 0) % 4][inverse_s_box[j]],
-                    t[(i + 1) % 4][inverse_s_box[j]],
-                    t[(i + 2) % 4][inverse_s_box[j]],
-                    t[(i + 3) % 4][inverse_s_box[j]]);
-            if (j != 255) fprintf(file, ", ");
-        }
-        fprintf(file, "},\n");
-    }
-
-    fprintf(file, "};\n");
-}
-
-static void print_InvMixColumns_table(FILE *file) {
-    fprintf(file, "const word InvMixColumns_table[4][256] = {\n");
-
-    const word m[4] = {0x0b, 0x0d, 0x09, 0x0e};
+static void print_MixColumns_table(FILE *file, const char *name, const word m[]) {
+    fprintf(file, "const word %s[4][256] = {\n", name);
 
     for (unsigned i = 0; i < 4; ++i) {
         fprintf(file, "    {");
         for (unsigned j = 0; j < 256; ++j) {
-            fprintf(file, "0x%02x%02x%02x%02x",
-                    multiply(m[(i + 0) % 4], j),
-                    multiply(m[(i + 1) % 4], j),
-                    multiply(m[(i + 2) % 4], j),
-                    multiply(m[(i + 3) % 4], j));
+            uword t;
+            for (unsigned k = 0; k < 4; ++k) {
+                t.bytes[3 - k] = multiply(j, m[(i + k) % 4]);
+            }
+            fprintf(file, "0x%08x", t.word);
             if (j != 255) fprintf(file, ", ");
         }
         fprintf(file, "},\n");
@@ -198,31 +136,45 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    initialise_s_box();
-    initialise_inverse_s_box();
+    word s_box[256];
+    initialise_s_box(s_box);
+
+    word inverse_s_box[256];
+    initialise_inverse_s_box(inverse_s_box);
 
     fprintf(file, "// This file was generated by makedata.c.\n");
+
     fprintf(file, "\n");
 
     fprintf(file, "#include \"aes.h\"\n");
+
     fprintf(file, "\n");
 
     print_Rcon(file);
+
     fprintf(file, "\n");
 
-    print_s_box(file);
+    // multipliers for MixColumns() transformation
+    const word m[4] = {0x03, 0x01, 0x01, 0x02};
+    const word inverse_m[4] = {0x0b, 0x0d, 0x09, 0x0e};
+
+    print_s_box(file, "s_box", s_box);
+
     fprintf(file, "\n");
 
-    print_inverse_s_box(file);
+    print_s_box(file, "inverse_s_box", inverse_s_box);
+
     fprintf(file, "\n");
 
-    print_cipher_table(file);
+    print_table(file, "cipher_table", s_box, m);
+
     fprintf(file, "\n");
 
-    print_inv_cipher_table(file);
+    print_table(file, "inv_cipher_table", inverse_s_box, inverse_m);
+
     fprintf(file, "\n");
 
-    print_InvMixColumns_table(file);
+    print_MixColumns_table(file, "InvMixColumns_table", inverse_m);
 
     fclose(file);
 
